5f2154eb6ee9f9fb2d0a099b72ebbd256269b5c5
Ana-ana <annapeshkova2013@gmail.com>
2020-08-11 22:33:55 +0300

    added diflib

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,7 @@
 import argparse
+import json
+import difflib
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file')
@@ -9,8 +12,46 @@ parser.add_argument('--f', '--format',
 
 
 def main():
-    parser.parse_args()
-    print('Hello')
+    args = parser.parse_args()
+    print('Magic begins..')
+    denerate_diff(Path(args.first_file).resolve(),
+                  Path(args.second_file).resolve())
+
+
+def denerate_diff(path_to_file1, path_to_file2):
+    with open(path_to_file1) as f:
+        file1 = f.readlines()  # json.load(f)
+    with open(path_to_file2) as q:
+        file2 = q.readlines() #json.load(q)
+    # print(file1)
+    # print()
+    # print(file2)
+    diff = difflib.Differ()
+    result = diff.compare(file1, file2)
+    result = [line for line in result if line.startswith(("- ", "+ ", " "))]
+    # print(type(result))
+    print(''.join(list(result)))
+    # print(list(result))
+    return result
+
+    # for key, value in file1.items():
+    #     for k, v in file2.items():
+    #         if key in file2.keys():
+    #             if file1.get(key) == file2.get(k):
+    #                 print('Common key: value')
+    #                 print('file1.get(key) {} == file2.get(k) {}'.format(file1.get(key),
+    #                                                                     file2.get(k)))
+    #                 print('key file1 {} and key file2 {}'.format(key, k))
+    #             else:
+    #                 print()
+    #                 print('else works')
+    #                 print('Value been changed')
+    #                 print('file1.get(key) {} == file2.get(k) {}'.format(file1.get(key),
+    #                                                                     file2.get(k)))
+    #                 print('key file1 {} and key file2 {}'.format(key, k))
+    #
+    # d = {x: file1[x] for x in file1 if x in file2}
+    # print(d)
 
 
 if __name__ == '__main__':
