2474decf8f0ffdd793e6030e97286fad9d21a633
Ana-ana <annapeshkova2013@gmail.com>
2020-08-12 10:39:44 +0300

    convert to ordered list

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -20,38 +20,32 @@ def main():
 
 def denerate_diff(path_to_file1, path_to_file2):
     with open(path_to_file1) as f:
-        file1 = f.readlines()  # json.load(f)
+        file1 = ordered(json.load(f))
     with open(path_to_file2) as q:
-        file2 = q.readlines() #json.load(q)
-    # print(file1)
-    # print()
-    # print(file2)
+        file2 = ordered(json.load(q))
     diff = difflib.Differ()
+    print(type(file1))
+    print(type(file2))
     result = diff.compare(file1, file2)
-    result = [line for line in result if line.startswith(("- ", "+ ", " "))]
-    # print(type(result))
-    print(''.join(list(result)))
-    # print(list(result))
+    # result = [line for line in result if not line.startswith(("? "))]
+    removing_symbols = ['(', ')']
+    print('\n'.join(result))
     return result
 
-    # for key, value in file1.items():
-    #     for k, v in file2.items():
-    #         if key in file2.keys():
-    #             if file1.get(key) == file2.get(k):
-    #                 print('Common key: value')
-    #                 print('file1.get(key) {} == file2.get(k) {}'.format(file1.get(key),
-    #                                                                     file2.get(k)))
-    #                 print('key file1 {} and key file2 {}'.format(key, k))
-    #             else:
-    #                 print()
-    #                 print('else works')
-    #                 print('Value been changed')
-    #                 print('file1.get(key) {} == file2.get(k) {}'.format(file1.get(key),
-    #                                                                     file2.get(k)))
-    #                 print('key file1 {} and key file2 {}'.format(key, k))
-    #
-    # d = {x: file1[x] for x in file1 if x in file2}
-    # print(d)
+
+def my_replace(string, removing_symbols):
+    result = ''.join([c for c in string if c not in removing_symbols])
+    print('result in my replace', result)
+    return result
+
+
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
 
 
 if __name__ == '__main__':
