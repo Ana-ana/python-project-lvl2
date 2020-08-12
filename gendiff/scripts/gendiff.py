f65614e5605bb04031373dd347427f7182abe1af
Ana-ana <annapeshkova2013@gmail.com>
2020-08-12 15:18:19 +0300

    added difference printing by for intead of diflib

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -20,32 +20,15 @@ def main():
 
 def denerate_diff(path_to_file1, path_to_file2):
     with open(path_to_file1) as f:
-        file1 = ordered(json.load(f))
+        file1 = json.load(f)
     with open(path_to_file2) as q:
-        file2 = ordered(json.load(q))
-    diff = difflib.Differ()
-    print(type(file1))
-    print(type(file2))
-    result = diff.compare(file1, file2)
-    # result = [line for line in result if not line.startswith(("? "))]
-    removing_symbols = ['(', ')']
-    print('\n'.join(result))
-    return result
-
-
-def my_replace(string, removing_symbols):
-    result = ''.join([c for c in string if c not in removing_symbols])
-    print('result in my replace', result)
-    return result
-
-
-def ordered(obj):
-    if isinstance(obj, dict):
-        return sorted((k, ordered(v)) for k, v in obj.items())
-    if isinstance(obj, list):
-        return sorted(ordered(x) for x in obj)
-    else:
-        return obj
+        file2 = json.load(q)
+    for (key, value) in set(file1.items()) & set(file2.items()):
+        print('   {}: {} in both'.format(key, value))
+    for (key, value) in set(file2.items()) - set(file1.items()):
+        print('+  {}: {} was added'.format(key, value))
+    for (key, value) in set(file1.items()) - set(file2.items()):
+        print('-  {}: {} was deleted'.format(key, value))
 
 
 if __name__ == '__main__':
