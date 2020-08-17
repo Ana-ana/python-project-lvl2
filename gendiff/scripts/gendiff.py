055ace58f8c6b3923c1f47aeedc4cce2c11270e6
Ana-ana <annapeshkova2013@gmail.com>
2020-08-17 18:01:02 +0300

    added test to script in travis.ci

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-from pathlib import Path
+
 from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Generate diff')
@@ -11,32 +11,30 @@ parser.add_argument('--f', '--format',
                     metavar='FORMAT',
                     help='set format of output')
 
+KEYS = ['common', 'changed', 'added', 'removed']
+
 
 def main():
     args = parser.parse_args()
-    print('\n'.join(render_result(
-                    generate_diff(
-                            get_data_from_file(Path(
-                                args.first_file).resolve()),
-                            get_data_from_file(Path(
-                                args.second_file).resolve())))))
+    print('\n'.join(render_result(generate_diff(
+        get_data_from_file(args.first_file),
+        get_data_from_file(args.second_file)))))
 
 
 def generate_diff(file_data1, file_data2):
-    keys = ['common ', 'changed ', 'added ', 'removed ']
     difference = defaultdict(dict)
-    for item in keys:
+    for item in KEYS:
         difference[item]
     for key in set(file_data1.keys()) & set(file_data2.keys()):
         if file_data1[key] == file_data2[key]:
-            difference['common '][key] = file_data1[key]
+            difference['common'][key] = file_data1[key]
         else:
-            difference['changed '][key] = [file_data2[key],
-                                           file_data1[key]]
+            difference['changed'][key] = [file_data2[key],
+                                          file_data1[key]]
     for key in set(file_data2.keys()) - set(file_data1.keys()):
-        difference['added '][key] = file_data2[key]
+        difference['added'][key] = file_data2[key]
     for key in set(file_data1.keys()) - set(file_data2.keys()):
-        difference['removed '][key] = file_data1[key]
+        difference['removed'][key] = file_data1[key]
     return difference
 
 
@@ -57,17 +55,22 @@ def check_path(path_to_file):
 
 def render_result(result_obj):
     rendered_result = ['{']
-    for k, v in result_obj.items():
-        for x, y in v.items():
-            if k == 'common ':
-                rendered_result.append('  {}: {}'.format(x, y))
-            elif k == 'changed ':
-                rendered_result.append('- {}: {}'.format(x, y[1]))
-                rendered_result.append('+ {}: {}'.format(x, y[0]))
-            elif k == 'added ':
-                rendered_result.append('+ {}: {}'.format(x, y))
-            elif k == 'removed ':
-                rendered_result.append('- {}: {}'.format(x, y))
+    for key_of_change, params in result_obj.items():
+        for file_key, file_key_value in params.items():
+            if key_of_change == 'common':
+                rendered_result.append('  {}: {}'
+                                       .format(file_key, file_key_value))
+            elif key_of_change == 'changed':
+                rendered_result.append('- {}: {}'
+                                       .format(file_key, file_key_value[1]))
+                rendered_result.append('+ {}: {}'
+                                       .format(file_key, file_key_value[0]))
+            elif key_of_change == 'added':
+                rendered_result.append('+ {}: {}'
+                                       .format(file_key, file_key_value))
+            elif key_of_change == 'removed':
+                rendered_result.append('- {}: {}'
+                                       .format(file_key, file_key_value))
     rendered_result.append('}')
     return rendered_result
 
