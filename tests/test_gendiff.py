a0b2c437f473195e72603ed735edc346a7307f67
Ana-ana <annapeshkova2013@gmail.com>
2020-08-17 18:36:59 +0300

    fix test coverage

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -19,10 +19,10 @@ def test_render_result():
 def test_generate_diff():
     before_dict = {"host": "hexlet.io", "timeout": 50, "proxy": "123.234.53.22"}
     after_dict = {"timeout": 20, "verbose": True, "host": "hexlet.io"}
-    expected_res = {'common ': {'host': 'hexlet.io'},
-                    'changed ': {'timeout': [20, 50]},
-                    'added ': {'verbose': True},
-                    'removed ': {'proxy': '123.234.53.22'}}
+    expected_res = {'common': {'host': 'hexlet.io'},
+                    'changed': {'timeout': [20, 50]},
+                    'added': {'verbose': True},
+                    'removed': {'proxy': '123.234.53.22'}}
     assert generate_diff(before_dict, after_dict) == expected_res
 
 
